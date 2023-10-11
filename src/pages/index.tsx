40f8ebd57cb0f672c01e70a5ea047bfad81621b1
killuh <20297127+killuhwhale@users.noreply.github.com>
2023-10-10 22:17:27 -0700

    target blank google play icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -93,12 +93,15 @@ const AppDownloadlinks: FC = () => {
         </Link>
         <div className="flex  max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20">
           <h3 className="text-2xl font-bold">Android →</h3>
-          <a href="https://play.google.com/store/apps/details?id=com.fitform&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1">
+          <Link
+            href="https://play.google.com/store/apps/details?id=com.fitform&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"
+            target="_blank"
+          >
             <img
               alt="Get it on Google Play"
               src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
             />
-          </a>
+          </Link>
         </div>
       </div>
     </div>
